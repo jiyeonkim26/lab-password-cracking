67d35b89e048d3a4d781fccbe9ebc1908171e183
Jiyeon Kim <jkim26@students.claremontmckenna.edu>
2026-02-26 18:30:32 -0800

    Delete password.py

--- password.py
+++
@@ -1,9 +0,0 @@
-from zipfile import ZipFile
-with ZipFile('guido_secrets.zip') as zf:
-    password = b'BFDL' 
-    # the above is equivalent to:
-    # password_str = 'BFDL'
-    # password = password_str.encode('ascii')
-    # because if we have a variable that is a str type, it can be encoded 
-    # into a bytes object with the .encode function into ascii
-    zf.extractall(pwd=password)
